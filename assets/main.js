5c81e68a498ba3dbe99084a483883fc3e2116b82
juanfernandez2306 <juanfernandez2306@gmail.com>
2022-03-23 09:14:29 -0400

    creando funcion grafico de metricas simples

--- assets/main.js
+++ assets/main.js
@@ -435,23 +435,334 @@ function stop_animation_loader(class_response, text_response){
     preloader.querySelector('h3').innerHTML = text_response;
 }
 
+function create_option_chart(){
+    return {
+        scales: {
+            x: {
+                stacked: false,
+                display: false,
+                title: {
+                    display: false,
+                    text: '',
+                    color: selectVariableCSS('--segundo-color'),
+                    font: {
+                        size: 18,
+                        family: selectVariableCSS('--font-body')
+                    }
+                },
+                ticks: {
+                    color: selectVariableCSS('--segundo-color')
+                }
+            },
+            y: {
+                stacked: false,
+                display: false,
+                title: {
+                    display: false,
+                    text: '',
+                    color: selectVariableCSS('--segundo-color'),
+                    font: {
+                        size: 18,
+                        family: selectVariableCSS('--font-body')
+                    }
+                },
+                ticks: {
+                    color: selectVariableCSS('--segundo-color')
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: true,
+                labels: {
+                    color: selectVariableCSS('--segundo-color'),
+                    font: {
+                        size: 16,
+                        family: selectVariableCSS('--font-body')
+                    }
+                }
+            },
+            title: {
+                display: true,
+                text: '',
+                color: selectVariableCSS('--segundo-color'),
+                font: {
+                    size: 18,
+                    family: selectVariableCSS('--font-body')
+                }
+            }
+        }
+    };
+}
+
+function calculate_index_axis_chart_bar(){
+    let intViewportWidth = window.innerWidth;
+    let indexAxis = 'y';
+
+    if(intViewportWidth < 600){
+        indexAxis = 'x';
+    }
+
+    return indexAxis;
+}
+
+
+function create_chart_pie({id_element, array_data, options}){
+    
+    let data = {
+        datasets: [{
+            data: array_data,
+            backgroundColor: [
+                selectVariableCSS('--firts-color-bar'),
+                selectVariableCSS('--third-color-bar')
+            ],
+            hoverOffset: 4
+        }],
+        labels: [
+            'Masculino',
+            'Femenino'
+        ]
+    };
+
+    let ctx = document.getElementById(id_element);
+    let myChart = new Chart(ctx, {
+        type: 'pie',
+        data: data,
+        options: options
+    });
+
+}
+
+function create_chart_stacked_bar({id_element, array_data_male, array_data_female, options}){
+
+    let data = {
+        labels: ['0-10', '11-20', '21-30', '31-40',
+                '41-50', '51-60', '> 61'],
+        datasets: [{
+          label: 'Masculino',
+          data: array_data_male,
+          backgroundColor: selectVariableCSS('--firts-color-bar'),
+          borderColor: selectVariableCSS('--segundo-color'),
+          borderWidth: 1
+        },
+        {
+          label: 'Femenino',
+          data: array_data_female,
+          backgroundColor: selectVariableCSS('--third-color-bar'),
+          borderColor: selectVariableCSS('--segundo-color'),
+          borderWidth: 1
+        }
+      ]
+    };
+
+    let ctx = document.getElementById(id_element);
+    let myChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: options
+    });
+
+}
+
+function create_chart_line({id_element, label_array, data_count, data_month, options}){
+    
+    const footer = (tooltipItems) => {
+        let month_value = '';
+      
+        tooltipItems.forEach(function(tooltipItem){
+          var index = tooltipItem.parsed.x;
+          var month_index_array = data_month[index];
+          month_value = array_month[month_index_array - 3];
+        });
+        return 'Mes: ' + month_value;
+    };
+
+    options.plugins.tooltip = {
+        callbacks: {
+            footer: footer
+        }
+    }
+
+    let data = {
+        labels: label_array,
+        datasets: [{
+            data: data_count,
+            borderColor: selectVariableCSS('--second-color-bar'),
+            pointBackgroundColor: selectVariableCSS('--second-color-bar'),
+            fill: false
+        }]
+    };
+
+    let ctx = document.getElementById(id_element);
+    let myChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: options
+    });
+}
+
+function create_char_bar(id_element, data_count, label_array){
+    let options = {
+        indexAxis: 'y',
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                display: true,
+                title: {
+                    display: true,
+                    text: '# de casos por 10.000 hab',
+                    color: selectVariableCSS('--segundo-color'),
+                    font: {
+                        size: 18,
+                        family: selectVariableCSS('--font-body')
+                    }
+                },
+                ticks: {
+                    color: selectVariableCSS('--segundo-color')
+                }
+            },
+            y: {
+                display: true,
+                title: {
+                    display: true,
+                    text: 'Municipios',
+                    color: selectVariableCSS('--segundo-color'),
+                    font: {
+                        size: 18,
+                        family: selectVariableCSS('--font-body')
+                    }
+                },
+                ticks: {
+                    color: selectVariableCSS('--segundo-color')
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false,
+                labels: {
+                    color: selectVariableCSS('--segundo-color'),
+                    font: {
+                        size: 16,
+                        family: selectVariableCSS('--font-body')
+                    }
+                }
+            },
+            title: {
+                display: true,
+                text: 'Distribución por municipios',
+                color: selectVariableCSS('--segundo-color'),
+                font: {
+                    size: 18,
+                    family: selectVariableCSS('--font-body')
+                }
+            },
+            subtitle: {
+                display: true,
+                text: 'Casos por 10.000 habitantes',
+                color: selectVariableCSS('--segundo-color'),
+                font: {
+                    size: 18,
+                    family: selectVariableCSS('--font-body')
+                }
+            } 
+        }
+    }
+
+    let data = {
+        labels: label_array,
+        datasets: [{
+          data: data_count,
+          fill: false,
+          borderColor: selectVariableCSS('--second-color-bar'),
+          backgroundColor: selectVariableCSS('--firts-color-bar')
+        }]
+    };
+
+    let ctx = document.getElementById(id_element);
+    let myChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: options
+    });
+}
+
+function load_simple_metrics(data_response){
+    let option_chart_pie = create_option_chart();
+        option_chart_pie.plugins.title.text = 'Distribución por sexo';
+    
+    create_chart_pie({
+        id_element: 'chart_pie_sex',
+        array_data: data_response.data.distribution_sex,
+        options: option_chart_pie
+    });
+
+    let option_chart_stacked_bar = create_option_chart();
+    option_chart_stacked_bar.plugins.title.text = 'Distribución por edad';
+    option_chart_stacked_bar.scales.x.stacked = true;
+    option_chart_stacked_bar.scales.x.display = true;
+    option_chart_stacked_bar.scales.x.title.display = true;
+    option_chart_stacked_bar.scales.x.text = '# de casos';
+
+    option_chart_stacked_bar.scales.y.stacked = true;
+    option_chart_stacked_bar.scales.y.display = true;
+    option_chart_stacked_bar.scales.y.title.display = true;
+    option_chart_stacked_bar.scales.y.text = 'Rango de edad (años)';
+
+    option_chart_stacked_bar.indexAxis = calculate_index_axis_chart_bar();
+    option_chart_stacked_bar.maintainAspectRatio = false;
+
+    create_chart_stacked_bar({
+        id_element: 'group_age',
+        array_data_male: data_response.data.distribution_age.male,
+        array_data_female: data_response.data.distribution_age.female,
+        options: option_chart_stacked_bar
+    })
+
+    let option_chart_line = create_option_chart();
+    option_chart_line.plugins.title.text = 'Distribución por semana';
+    option_chart_line.scales.x.display = true;
+    option_chart_line.scales.x.title.display = true;
+    option_chart_line.scales.x.title.text = 'Semana epidemiologica';
+
+    option_chart_line.scales.y.display = true;
+    option_chart_line.scales.y.title.display = true;
+    option_chart_line.scales.y.title.text = '# de casos';
+
+    option_chart_line.plugins.legend.display = false;
+
+    create_chart_line({
+        id_element: 'week_epi',
+        label_array: data_response.data.distribution_week_epi.label,
+        data_count: data_response.data.distribution_week_epi.count,
+        data_month: data_response.data.distribution_week_epi.month,
+        options: option_chart_line
+    })
+    
+
+}
+
 function load(){
 
     let FormData_input = new FormData();
-    FormData_input.append('number_month', 3);
+    FormData_input.append('number_month', 12);
 
     selectElement('#restart_preloader').addEventListener('click', restart_style_preloader_wallpaper, false);
-
+    
     Promise.all([
         get_post_data_json(url_post_array_community, FormData_input),
-        get_data_json('assets/php/create_data_dpt.php')
+        get_data_json('assets/php/create_data_dpt.php'),
+        get_post_data_json('assets/php/load_simple_metrics.php', FormData_input)
     ])
     .then((array_response_data) =>{
         let array_data_point = array_response_data[0];
         let data_dpt = array_response_data[1];
+        let data_metrics = array_response_data[2];
         let geojson = create_geojson(array_data_point.data);
+
+        load_simple_metrics(data_metrics);
         
-        create_map(geojson, data_dpt);
+        //create_map(geojson, data_dpt);
     })
     .catch((error) => {
 		console.log(error);
